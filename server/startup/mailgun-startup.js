3911f88f7be48253ea9276eca02c94b58b9eab4e
Pedro Luis Furio Raphael <pedroluis.raphael@gmail.com>
2016-05-16 18:04:15 -0300

    changed email credentials

--- server/startup/mailgun-startup.js
+++ server/startup/mailgun-startup.js
@@ -1,10 +1,10 @@
 Meteor.startup(function () {
 
-  process.env.MAIL_URL = "smtp://postmaster@app.cordvida.com.br:0c5d8205541df93e00a26ce04c2ec145@smtp.mailgun.org:587";
+  process.env.MAIL_URL = "smtp://admin@app.cordvida.com.br:supcord123@smtp.mailgun.org:587";
 
   console.log('instantianting mailgun credentials');
   var options = {
-    apiKey: 'key-28085066666d237635d3d1cefe098991',
+    apiKey: 'key-56dc5763b21c1e1393c8eed36eeaf935',
     domain: 'app.cordvida.com.br'
   }
   CordvidaMailgun = new Mailgun(options);
